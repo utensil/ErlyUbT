545c045b6ae9bbfdbbf179a0cc6de41ac09c13dc
unknown <Administrator@.(none)>
2009-06-06 10:23:39 +0800

    now the server can be stopped

--- test/demo_udp.erl
+++ test/demo_udp.erl
@@ -1,8 +1,8 @@
 -module(demo_udp).
--export([start_server/0, server/1, client/1]).
+-export([start_server/0, server/1, client/1, stop_server/0]).
 
 start_server() ->
-    spawn(fun() -> server(4000) end).
+    spawn(fun() -> server(3681) end).
 %% The server
 server(Port) ->
     {ok, Socket} = gen_udp:open(Port, [binary]),
@@ -13,10 +13,14 @@ loop(Socket) ->
     receive
         {udp, Socket, Host, Port, Bin} = Msg ->
             io:format("server received:~p~n" ,[Msg]),
-            N = binary_to_term(Bin),
-            Fac = fac(N),
-            gen_udp:send(Socket, Host, Port, term_to_binary(Fac)),
-            loop(Socket)
+            case binary_to_term(Bin) of
+                close ->
+                    gen_udp:close(Socket);
+                N ->
+                    Fac = fac(N),
+                    gen_udp:send(Socket, Host, Port, term_to_binary(Fac)),
+                    loop(Socket)
+            end
     end.
 
 fac(0) -> 1;
@@ -26,10 +30,11 @@ fac(N) -> N * fac(N-1).
 client(N) ->
     {ok, Socket} = gen_udp:open(0, [binary]),
     io:format("client opened socket=~p~n" ,[Socket]),
-    ok = gen_udp:send(Socket, "localhost" , 4000,
+    ok = gen_udp:send(Socket, "localhost" , 3681,
     term_to_binary(N)),
     Value = receive
-                {udp, Socket, _, _, Bin} = Msg ->
+                {udp, Socket, A, B, Bin} = Msg ->
+                io:format("~p~n", [[A, B]]),
                 io:format("client received:~p~n" ,[Msg]),
                 binary_to_term(Bin)
             after 2000 ->
@@ -37,4 +42,8 @@ client(N) ->
             end,
     gen_udp:close(Socket),
     Value.
+    
+stop_server() ->
+    {ok, Socket} = gen_udp:open(0, [binary]),
+    ok = gen_udp:send(Socket, "localhost" , 3681, term_to_binary(close)).
     
