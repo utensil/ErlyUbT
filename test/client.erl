cf411a773533c9e94308e453122afea54867e29d
unknown <Administrator@.(none)>
2009-06-06 09:13:08 +0800

    put the tcp client and server into one module

--- test/client.erl
+++
@@ -1,9 +0,0 @@
--module(client).
--compile(export_all).
-%-export([client/0]).
-
-client() ->
-    {ok, Sock} = gen_tcp:connect("localhost", 3561, [binary, {packet, 0}]),
-    {ok, Data} = file:read_file("restproject.swf"),
-    ok = gen_tcp:send(Sock, Data),
-    ok = gen_tcp:close(Sock).
