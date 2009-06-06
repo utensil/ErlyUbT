283649514d5d56725c5521535f8ee3df6147990f
unknown <Administrator@.(none)>
2009-06-06 10:01:11 +0800

    add param Port for server and client.

--- test/demo_tcp.erl
+++ test/demo_tcp.erl
@@ -1,11 +1,11 @@
 -module(demo_tcp).
--export([start_server/0, server/0, client/0]).
+-export([start_server/0, server/1, client/1]).
 
 start_server() ->
-    spawn(fun() -> server() end).
+    spawn(fun() -> server(3561) end).
 
-server() ->
-    {ok, LSock} = gen_tcp:listen(3561, [binary, {packet, 0},
+server(Port) ->
+    {ok, LSock} = gen_tcp:listen(Port, [binary, {packet, 0},
                                         {active, false}]),
     {ok, Sock} = gen_tcp:accept(LSock),
     {ok, Bin} = do_recv(Sock, []),
@@ -22,8 +22,8 @@ do_recv(Sock, Bs) ->
             {ok, list_to_binary(Bs)}
     end.
 
-client() ->
-    {ok, Sock} = gen_tcp:connect("localhost", 3561, [binary, {packet, 0}]),
+client(Port) ->
+    {ok, Sock} = gen_tcp:connect("localhost", Port, [binary, {packet, 0}]),
     {ok, Data} = file:read_file("restproject.swf"),
     ok = gen_tcp:send(Sock, Data),
     ok = gen_tcp:close(Sock).
