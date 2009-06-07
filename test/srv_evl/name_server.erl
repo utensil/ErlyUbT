26cb4c2a9af5305873924313bfbc251372e0743e
unknown <Administrator@.(none)>
2009-06-07 08:39:10 +0800

    done with server2, introduced a func to casue exception.

--- test/srv_evl/name_server.erl
+++ test/srv_evl/name_server.erl
@@ -1,12 +1,14 @@
 -module(name_server).
--export([init/0, add/2, whereis/1, handle/2]).
--import(server1, [rpc/2]).
+-export([init/0, add/2, whereis/1, exception/1, handle/2]).
+-import(server2, [rpc/2]).
 
 %% client routines
 add(Name, Place) ->
    rpc(name_server, {add, Name, Place}).
 whereis(Name) ->
    rpc(name_server, {whereis, Name}).
+exception(N) ->
+   rpc(name_server, {exception, N}).
 
 %% callback routines
 init() -> 
@@ -14,5 +16,16 @@ init() ->
 
 handle({add, Name, Place}, Dict) ->
     {ok, dict:store(Name, Place, Dict)};
+
 handle({whereis, Name}, Dict) ->
-    {dict:find(Name, Dict), Dict}.
+    {dict:find(Name, Dict), Dict};
+% to test if the server can handle exception
+handle({exception, N}, _) ->
+    case N of
+        1 -> n;
+        2 -> throw(n);
+        3 -> exit(n);
+        4 -> {'EXIT', n};
+        5 -> erlang:error(n);
+        0 -> 1/N
+   end.
