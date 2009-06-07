0cf380eb16f0835b8ee957297ca1c72705b47e1d
unknown <Administrator@.(none)>
2009-06-07 10:05:03 +0800

    server5: become fun my_fac_server:loop/0. Interesting.

--- test/srv_evl/my_fac_server.erl
+++ test/srv_evl/my_fac_server.erl
@@ -1,1 +1,25 @@
 -module(my_fac_server).
+-export([loop/0]).
+
+loop() ->
+    receive
+        {From, {fac, N}} ->
+            From ! {self(), fac(N)},
+            loop();
+        {become, Something} ->
+            Something()
+    end.
+
+fac(0) -> 1;
+fac(N) -> N * fac(N-1).
+
+%15> c(server5).
+%{ok,server5}
+%16> c(my_fac_server).
+%{ok,my_fac_server}
+%17> Pid = server5:start().
+%<0.71.0>
+%18> Pid ! {become, fun my_fac_server:loop/0}.
+%{become,#Fun<my_fac_server.loop.0>}
+%19> server5:rpc(Pid, {fac, 30}).
+%265252859812191058636308480000000
--- test/srv_evl/server5.erl
+++ test/srv_evl/server5.erl
@@ -1,1 +1,16 @@
 -module(server5).
+-export([start/0, rpc/2]).
+
+start() ->
+    spawn(fun() -> wait() end).
+
+wait() ->
+    receive
+        {become, F} -> F()
+    end.
+
+rpc(Pid, Q) ->
+    Pid ! {self(), Q},
+    receive
+        {Pid, Reply} -> Reply
+    end.
