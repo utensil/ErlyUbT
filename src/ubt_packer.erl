46e4f2be8dd2bc82e6d64a27468fd1cd3a45cc9d
unknown <Administrator@.(none)>
2009-06-07 14:39:13 +0800

    The packing and unpacking of UbT packet.

---
+++ src/ubt_packer.erl
@@ -0,0 +1,76 @@
+-module(ubt_packer).
+-export([unpack/1, pack/1]).
+
+-define(N_32, 32/unsigned-big-integer).
+-define(N_16, 16/unsigned-big-integer).
+-define(N_8, 8/unsigned-big-integer).
+
+-record(udp_header,
+    {
+        src_port,
+        dst_port,
+        seq_no,
+        ack_no,
+        syn,
+        ack,
+        rst,
+        fin,
+        wndw
+    }).
+
+unpack(<<SrcPort : ?N_16,
+      DstPort : ?N_16,
+      SeqNo : ?N_32,
+      AckNo : ?N_32,
+      DataOffset :4,
+      Reserved :6,
+      %cflag begin
+      Urg :1,
+      Syn :1,
+      Ack :1,
+      Rst :1,
+      Psh :1,
+      Fin :1,
+      %cflag end
+      Wndw :?N_16,
+      Checksum :?N_16,
+      Urg_ptr :?N_16,
+      Rest/binary>>) ->
+      Header = #udp_header{
+            src_port = SrcPort,
+            dst_port = DstPort,
+            seq_no = SeqNo,
+            ack_no = AckNo,
+            syn = Syn,
+            ack = Ack,
+            rst = Rst,
+            fin = Fin,
+            wndw = Wndw
+        },
+      { Header, Rest }.
+
+pack({{ Header, Rest }}) ->
+    {SrcPort, DstPort, SeqNo, AckNo, Syn, Ack, Rst, Fin, Wndw}
+        = Header,
+    { DataOffset, Reserved, Urg, Psh, Checksum, Urg_ptr}
+        = { 0, 0, 0, 0, 0, 0},
+    <<SrcPort : ?N_16,
+      DstPort : ?N_16,
+      SeqNo : ?N_32,
+      AckNo : ?N_32,
+      DataOffset :4,
+      Reserved :6,
+      %cflag begin
+      Urg :1,
+      Syn :1,
+      Ack :1,
+      Rst :1,
+      Psh :1,
+      Fin :1,
+      %cflag end
+      Wndw :?N_16,
+      Checksum :?N_16,
+      Urg_ptr :?N_16,
+      Rest/binary>>.
+
+     
