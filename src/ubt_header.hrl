a3b876d7a472e2927deb0f979d3cf0bab9c945c9
unknown <Administrator@.(none)>
2009-06-12 11:18:10 +0800

    finished the design of send and receive data structure

--- src/ubt_header.hrl
+++ src/ubt_header.hrl
@@ -22,6 +22,36 @@
     {
         l_sock,
         r_addr = none,
-        r_port = none
+        r_port = none,
+        timout = {}, %suppose to be a timout_struct
+        %%timestamp is for each MSS
+        cong_win = 0,
+        %%buff, %array? no, use binary match
+        %% buffer size...
+        win = 4096,
+        % snd_buf  snd_una          snd_nxt
+        %  |            |                |           |
+        %  |  acked     | sent, unacked  |           |
+        %  |            |             win            | buffer_size
+        snd_buf,
+        snd_una, % snd_win_left
+        snd_nxt, % snd_una + win = snd_win_right
+        % rcv_buf  rcv_wup          snd_nxt
+        %  |             |                |           |
+        %  | rcvd&acked  | rcvd, unacked  |           |
+        %  |             |             win            | buffer_size
+        rcv_buf,
+        rcv_wup,  % last window update
+        rcv_nxt
+
     }
 ).
+
+% a block is created only after it's sent, and it will be
+-record(ubt_block,
+    {
+        % we don't worry about the beginning, if end_seq + 1 < ack_no, this
+        % block is dequeued, and timeout are calculated
+        end_seq, 
+        snd_timestamp
+    }).
